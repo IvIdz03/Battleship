c7a5384c8f087d0f01b9def30a760cdb311b7fbb
ivani <ividz@vsite.hr>
2025-06-03 16:57:59 +0200

    tocna verzijapromjena koda koji se rusio

--- Model/LimitedQueue.cs
+++ Model/LimitedQueue.cs
@@ -1,9 +1,20 @@
-﻿namespace Model;
+﻿using System.Collections;
 
-public class InlineTargetSelector : ITargetSelector
+namespace Model;
+
+public class LimitedQueue<T> : Queue<T>
 {
-    public Square Next()
+    private readonly int _maxItems;
+    public LimitedQueue(int maxItems)
     {
-        throw new NotImplementedException();
+        _maxItems = maxItems;
+    }
+    public new void Enqueue(T item)
+    {
+        while (Count >= _maxItems)
+        {
+            Dequeue();
+        }
+        base.Enqueue(item);
     }
 }
